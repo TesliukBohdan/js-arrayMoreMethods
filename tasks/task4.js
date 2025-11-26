65c4687090d39ff9dcd9eb886eb2832421821f40
TesliukBohdan <kn24-b.tesliuk@nubip.edu.ua>
2025-11-26 18:39:07 +0200

    Update task4.js

--- tasks/task4.js
+++ tasks/task4.js
@@ -3,8 +3,15 @@
 // Якщо це так, то фільтруємо машини по бренду і повертаємо ті, що найбільш економні.
 
 function getMostFuelEfficientCars(cars, brand) {
-  // Ваш код
+  if (!cars.every(car => car.fuelConsumption < 6)) return [];
+
+  return cars
+    .filter(car => car.brand === brand && car.fuelConsumption < 6)
+    .sort((a, b) => a.fuelConsumption - b.fuelConsumption);
 }
+
+module.exports = getMostFuelEfficientCars;
+
 
 // Приклад використання:
 const cars = [
